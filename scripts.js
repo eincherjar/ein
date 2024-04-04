655e337a7757804f5bd6553967eb089eadc547c0
Marcin Barszcz <marcinbarszcz2@gmail.com>
2024-04-04 21:13:16 +0200

    init ein

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+let root = document.querySelector(":root");
+let button = document.querySelector("#change-collors");
+
+button.addEventListener("click", () => {
+  event.preventDefault();
+  root.classList.toggle("theme-dark");
+});
